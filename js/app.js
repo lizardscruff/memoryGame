21edc6d37e4c88da1d31754217fb4d05dd54795a
8bit <lizardscruff@users.noreply.github.com>
2017-09-24 21:02:08 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /*
  * This list has all of the card's names.
+ * The rest are variables for shortcuts.
  */
 var cardNames  = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf',
 				  'bicycle', 'bomb','diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube',
@@ -8,7 +9,8 @@ var cardNames  = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf',
 	deck       = $('.deck'),
 	card       = $('.card'),
 	cardSymbol = card.children(),
-	moves	   = $('.moves');
+	movesClass = $('.moves'),
+	restart	   = $('.restart');
 	
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -27,57 +29,110 @@ function shuffle(array) {
 
 // This is the game board
 function startGame() {
-	// This holds opened cards that are ready to be 
-	// checked against the next clicked card.
-	var openCards = [];
+
+	// This array holds the first clicked card and second clicked card.
+	// The rest of the variables are for shortcut and later functions.
+	var openCards  = [],
+		moves 	   = 0,
+		matches    = 0,
+		modal	   = document.getElementById('win-modal'),
+		span 	   = document.getElementById('close');
 
 	// This randomizes the face-down cards.
 	shuffle(cardNames);
 	cardSymbol.removeClass();
-	// ??? Helper said something about not needing index? But it doesn't work without it.
+
+	// ??? Helper said something about not needing index here? But it doesn't work without it.
 	cardSymbol.each(function(index) {
 		$( this ).addClass('fa fa-' + cardNames[index]);
 		index++;
 	});
 
-	// Flips one card and then shuts off.
-	function flipOnce() {
+	// This starts listening for clicks on the deck and cards.
+	function initEventListener() {
+		// This makes sure the click listener is off for the next cycle.
+		deck.off('click');
+
+		// Check if matches have been met.
+		endGame(matches);
 		
-		// Pfft. Why does it only work once. It gets stuck after this <------------------
+		// This makes the restart button flip everything back down and sets values to zero.
+		restart.click(function() {
+			card.removeClass();
+			card.addClass('card');
+			matches = 0;
+			moves = 0;
+			movesClass.text(moves);
+		});
+   
+    // attach a click event listener to the card elements
 		deck.on('click', '.card', function() {
+			moveCounter();
+			console.log(moves + "moves.");
+			movesClass.text(moves);
+
 			// Flips the card from face-down to face-up.
 			var flipped = $( this ).addClass('open show');
-			// Stops the event.
-			deck.off('click');
-			console.log('flippedOnce')
-		});
-		
-		/*
-		card.click(function() {
-			// Flips the card from face-down to face-up.
-			var flipped = $( this ).addClass('open show');
-			// Puts the card's symbol name into the openCards array.
-			openCards.push(flipped.children().attr('class'));
-			// Stops the event.
-			card.off('click');
-			console.log('flippedOnce')
-			console.log(openCards);
-		});
-		*/
 
+			// Adds clicked card to the array to be checked later for matches.
+			openCards.push(flipped.children());
+
+			if (openCards.length > 1) {
+				var compare = compareCards(openCards);
+				console.log(compare);
+				if (compare === false) {
+					for (var index = 0; index < 2; index++) {
+						$(openCards[index]).parent().addClass('animated flash').css('background', '#ff3300');				
+					}
+
+					//Found on: http://www.telegraphicsinc.com/2013/07/how-to-use-animate-css/
+					window.setTimeout( function(){
+						card.removeClass('open show animated flash').css('background', '');
+					}, 750);
+
+					// Clears the array to keep the code from breaking.
+					openCards.splice(0, 2);
+
+					initEventListener();
+				} else {
+					lockMatch();				
+				}
+			}
+		});
 	}
 
-	/*
-	// Puts the card's symbol name into the openCards array.
-	function addToArray(array) {
-		openCards.push(flipped.children().attr('class'));
-		return array;
+	// This increases moves by 1.
+	function moveCounter() {
+		moves++;
 	}
-	*/
 
-	// This compares the two clicked cards to see if they are the same and returns boolean.
+	// When 8 matches are made this function executes and brings up the victory modal!
+	function endGame(value) {
+		if (value === 8) {
+			// Opens the modal.
+			// https://www.w3schools.com/howto/howto_css_modals.asp
+			modal.style.display = 'block';
+
+			// https://stackoverflow.com/questions/9778888/uncaught-typeerror-cannot-set-property-onclick-of-null
+			window.onload = function() {
+				// When 'x' is clicked, the modal closes.
+				$('.close').on('click', function() {
+					modal.style.display = 'none';
+				});
+
+				// When outside the window is clicked, the modal closes.
+				window.onclick = function(event) {
+					if (event.target == modal) {
+						modal.style.display = 'none';
+					}
+				}
+			}
+		}
+	}
+
+	// This compares the two clicked cards to see if they are the same and returns a boolean.
 	function compareCards(array) {
-		if (openCards[0] === openCards[1]) {
+		if (openCards[0].attr('class') === openCards[1].attr('class')) {
 			return true;
 		} else {
 			return false;
@@ -86,134 +141,20 @@ function startGame() {
 
 	// This function will lock the matches.
 	function lockMatch() {
-		// code
-	}
-
-	// This flips the two unmatched cards back over. UNTESTED
-	function flipBackOver(array) {
-		deck.children().removeClass('open show');
-		array = [];
-		return array;
-	}
-
-	// Flips the card from face-down to face-up.
-	function cardFlipper() {
-		if (openCards.length < 1) {
-			// How do I get this to run first and then 
-			// wait till it's done before it does the next step?
-			flipOnce();
-			// WHY DOES THIS RUN FIRST!?
-			console.log("this should run after the card is flipped!");
-		} else {
-			flipOnce();
-			var compared = compareCards(openCards);
-			console.log(compared);
-			if (compared === true) {
-				lockMatch();
-			} else {
-				flipBackOver(openCards);
-			}
+		for (var index = 0; index < 2; index++) {
+			$(openCards[index]).parent().removeClass('open show');
+			$(openCards[index]).parent().addClass('match');
+			$(openCards[index]).parent().addClass('animated tada');
 		}
+		// Empties the array so the code doesn't break.
+		openCards.splice(0, 2);
+		// Increase matches, which is in the startGame() scope.
+		matches++;
+		deck.off('click');
+		initEventListener();
 	}
-	cardFlipper();	
+
+	initEventListener();	
 }
 
 startGame();
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-// *** Stuff that does work: ***
-
-/*
-.removeClass();
-.addClass('match');
-
-card.click(function() {
-	$( this ).addClass('open show');
-});
-
-	// FLips forever.
-	function flipForever() {
-		card.click(function() {
-			var flippy = $( this ).addClass('open show');
-			openCards.push(flippy.children().attr('class'));
-			console.log(openCards);
-			console.log('flippin forever!')
-		});
-	}
-
-
-*/
-
-// Stuff that doesn't work:
-
-/*
-function compareCards(variable, array) {
-		if (flipped === array[0]) {
-			console.log('This is a test. Success!')
-		} else {
-			return array;
-		}
-		return array;
-	}
-
-
-	function compareCards() {
-		console.log('Shit!');
-		
-		if (flipped === openCards[0]) {
-			$( this ).removeClass('open show');
-			$( this ).addClass('match');
-		} else {
-			$( this ).removeClass('open show')
-		}
-*/
-
-/*
-var flippedClass = flipped.children().attr('class');
-*/
-
-// This switches the card to 'card open show' when clicked
-/*
-function cardFlipper() {
-	card.click(function() {
-		$( this ).***class is lowercase dink***addclass( 'open show' );
-	});
-}
-*/
-
-/*
-function cardFlipper(openCards) {
-	if (openCards.length === 0){
-		card.click(function() {
-			var flipped = $( this ).addClass( 'open show' );
-			openCards.push(flipped.children());
-			});
-		}
-	}
-*/
-
-// Each time a card is opened it goes in the openCards array.
-	/*
-	*YOU FORGOT THE VAR* openCards = [];
-	if (openCards.length() === 0) {
-		card.click(function() {
-			$( this ).add*CLASS SHOULD BE UPPERCASEclass( 'open show' )
-		});
-	} else {
-		console.log("It's full mang");
-	}
-	*/
-
-// This sets moves to 0. Currently not editable.
-// :/ Actually this doesn't even work at all.
-// moves.text('0');
